b5168f7a196ce8f4a379b7d0f8c0a2c67e255e27
MindInitiatives <stevyn258@yahoo.com>
2020-07-08 15:21:02 +0100

    Second commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb+srv://test:test@todo-htlrq.mongodb.net/test?retryWrites=true&w=majority',
+    // 'mongodb://localhost:27017/meanauth',
     secret: 'yoursecret'
 }
